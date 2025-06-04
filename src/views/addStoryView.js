23159c517419f6990fbf13409be4418f6d978084
Firaniran <firaninuranjani@gmail.com>
2025-06-04 17:19:42 +0700

    fix icon leafet

--- src/views/addStoryView.js
+++ src/views/addStoryView.js
@@ -2,6 +2,13 @@
 import Swal from 'sweetalert2';
 import L from 'leaflet';
 
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+});
+
 export default class AddStoryView {
   constructor() {
     this.container = null;
