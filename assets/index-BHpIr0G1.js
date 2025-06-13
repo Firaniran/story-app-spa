0f27e42af9107d26ebe2198774a1e40f18ff95b6
Firaniran <firaninuranjani@gmail.com>
2025-06-13 22:31:48 +0700

    Updates

--- assets/index-BHpIr0G1.js
+++ assets/index-BHpIr0G1.js
@@ -42,24 +42,22 @@
               </li>
               `:""}
 
-              ${o?"":`
-              <li role="none">
-                <a role="menuitem" href="#/login" class="nav-link" tabindex="0">
-                  <svg class="nav-icon" viewBox="0 0 24 24" aria-hidden="true">
-                    <path d="M11 7L9.6 8.4l2.6 2.6H2v2h10.2l-2.6 2.6L11 17l5-5-5-5z"/>
-                  </svg>
-                  <span>Masuk</span>
-                </a>
-              </li>
-              <li role="none">
-                <a role="menuitem" href="#/register" class="nav-link" tabindex="0">
-                  <svg class="nav-icon" viewBox="0 0 24 24" aria-hidden="true">
-                    <path d="M15 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm-9-2V8c0-.55-.45-1-1-1s-1 .45-1 1v2H2c-.55 0-1 .45-1 1s.45 1 1 1h2v2c0 .55.45 1 1 1s1-.45 1-1v-2h2c.55 0 1-.45 1-1s-.45-1-1-1H6z"/>
-                  </svg>
-                  <span>Daftar</span>
-                </a>
-              </li>
-              `}
+            ${o?`
+           <aside class="user-actions" aria-label="Aksi pengguna" style="display: flex; align-items: center; gap: 1rem;">
+            <button id="pushButton" class="push-btn" type="button" aria-label="Aktifkan Notifikasi" style="background: none; border: none; cursor: pointer;">
+              <svg class="nav-icon" viewBox="0 0 24 24" width="24" height="24" fill="currentColor" aria-hidden="true">
+              <path d="M12 22c1.1 0 2-.9 2-2h-4a2 2 0 0 0 2 2zm6-6v-5c0-3.07-1.63-5.64-4.5-6.32V4a1.5 1.5 0 0 0-3 0v.68C7.63 5.36 6 7.92 6 11v5l-1.99 2H20l-2-2z"/>
+                </svg>
+                  </button>
+                <button id="logoutBtn" class="logout-btn" role="menuitem" tabindex="0" type="button" aria-label="Keluar dari akun">
+              <svg class="logout-icon" viewBox="0 0 24 24" aria-hidden="true">
+                <path d="M16 17v-3H9v-4h7V7l5 5-5 5zM14 2a2 2 0 0 1 2 2v2h-2V4H5v16h9v-2h2v2a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9z"/>
+                </svg>
+             <span>Keluar</span>
+              </button>
+            </aside>
+            `:""}
+
             </ul>
 
             ${o?`
