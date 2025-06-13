895ccf25b37f998a5aa81b66e92ccaaf0b7b8401
Firaniran <firaninuranjani@gmail.com>
2025-06-13 20:47:50 +0700

    fix push notif

--- src/main.js
+++ src/main.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   if ('serviceWorker' in navigator) {
     try {
-      await navigator.serviceWorker.register('/service-worker.js');
+      await navigator.serviceWorker.register(`${import.meta.env.BASE_URL}service-worker.js`);
       console.log('Service Worker registered');
     } catch (error) {
       console.error('Service Worker registration failed:', error);
